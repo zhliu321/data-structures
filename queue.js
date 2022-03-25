a3ab1e3615b087ee10634c89aeea6ed1cd7fae29
Zi Han Liu <zhliu321@gmail.com>
2022-03-25 12:17:37 -0400

    Queue implementation in js

---
+++ queue.js
@@ -0,0 +1,51 @@
+// FIFO - First In First Out
+class QueueNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new QueueNode(val);
+    if (this.size === 0) {
+      this.front = newNode;
+      this.back = newNode;
+    }
+    else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    if (this.size === 0) return null;
+    const removedNode = this.front;
+    if (this.size === 1) {
+      this.back = null;
+    }
+    this.front = this.front.next;
+    this.size--;
+    return removedNode.val;
+  }
+}
+
+// const myQueue = new Queue();
+// myQueue.enqueue('a');
+// myQueue.enqueue('b');
+// myQueue.enqueue('c');
+// myQueue.dequeue();
+// console.log(myQueue);
+// console.log(myQueue.size);
+// myQueue.enqueue('d');
+// myQueue.enqueue('e');
+// console.log(myQueue);
+// console.log(myQueue.size);
