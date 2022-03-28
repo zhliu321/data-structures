b72f619dbbf17ae79121d5b673dce07e0e9b496a
Zi Han Liu <zhliu321@gmail.com>
2022-03-28 09:47:18 -0400

    Recursive combinations implementation in js

---
+++ combinations.js
@@ -0,0 +1,21 @@
+const combinations = (elements) => {
+  if (elements.length == 0) return [ [] ];
+
+  const firstEl = elements[0];
+  const rest = elements.slice(1);
+  const combsWithoutFirst = combinations(rest);
+  const combsWithFirst = [];
+  
+  combsWithoutFirst.forEach(comb => {
+    const combWithFirst = [...comb, firstEl];
+    combsWithFirst.push(combWithFirst);
+  });
+
+  return [ ...combsWithoutFirst, ...combsWithFirst ];
+};
+
+// Time: O(2^n)
+// Space: O(n^2)
+
+console.log(combinations(['a', 'b', 'c']));
+console.log(combinations(['a', 'b', 'c', 'd', 'e', 'f']));
